21c77eed9a1c1a2594396525063ce38547951254
Praveen <praveen.b@outlook.com>
2019-03-08 16:17:19 +0000

    initial commit.

---
+++ UserManagementApi/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementApi
+{
+    public static class Constants
+    {
+        public const string ScopeClaimType = "http://schemas.microsoft.com/identity/claims/scope";
+        public const string ObjectIdClaimType = "http://schemas.microsoft.com/identity/claims/objectidentifier";
+    }
+}
---
+++ UserManagementApi/Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using UserManagementApi.Authorization;
+
+namespace UserManagementApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(config =>
+            {
+                config.Filters.Add(new AuthorizeFilter("default"));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(options =>
+            //{
+            //    options.Authority = Configuration["Authentication:Authority"];
+            //    options.Audience = Configuration["Authentication:ClientId"];
+            //    options.TokenValidationParameters.ValidateLifetime = true;
+            //    options.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+            //});
+
+            //services.AddAuthorization();
+
+            services.AddAuthorization(o =>
+            {
+                o.AddPolicy("default", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    //.RequireClaim(Constants.ScopeClaimType, "user_impersonation");
+                });
+            });
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(o =>
+            {
+                o.Authority = Configuration["Authentication:Authority"];
+                o.SaveToken = true;
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidAudiences = new List<string>
+                    {
+                        Configuration["Authentication:AppIdUri"],
+                        Configuration["Authentication:ClientId"]
+                    }
+                };
+            });
+
+            //// Add claims transformation to split the scope claim value
+            services.AddSingleton<IClaimsTransformation, AzureAdScopeClaimTransformation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Very important that this is before MVC (or anything that will require authentication)
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
